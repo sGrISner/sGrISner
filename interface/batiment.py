d37b76c215b4e43585a37229b7a1e2a7cfd981e3
CHUPClem <clemence.chup1@gmail.com>
2018-01-19 12:09:43 +0100

    building ok

---
+++ interface/batiment.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+# Python v3
+
+""" - FICHIER DE DEFINITION DE LA CLASSE BATIMENT - """
+
+class Batiment:
+    """
+    ------ OBJET BATIMENT ------
+
+    ATTRIBUTS :
+    ===========
+        - identity = identifiant de l'entité
+        - geometry = géométrie de l'entité
+        - classe = classe de l'entité
+        - probability = probabilité d'appartenance à la classe
+    """
+
+    def __init__(self, identity, geometry, classe, probability):
+        self.identity = identity
+        self.geometry = geometry
+        self.classe = classe
+        self.probability = probability
