5fede42b1362c28501791601deb5f74f396a2de6
Ethiy <oussama.ennafii@ign.fr>
2018-02-13 17:49:33 +0100

    new ui main window

--- sgrisner.py
+++ sgrisner.py
@@ -10,6 +10,7 @@ from PyQt5.QtGui import QPixmap, QPolygonF
 import PyQt5.QtCore
 
 from lib.classificationActive import *
+from lib.mainWindow import Ui_mainWindow
 from lib.choixClasse import *
 from lib.chargementFichiers import *
 
@@ -35,12 +36,12 @@ class LoaderWindow(QDialog, Ui_ChargerFichier):
     METHODES:
     ==========
         - select_classes: demande le chemin de la classe
-        - select_results: demande le chemin du fichier de résultats
+        - select_entries: demande le chemin du fichier de résultats
         - select_background: demande le chemin de l'orthoimage
         - select_buildings_dir: demande le chemin du dossier des emprises
         - param_stratregy: activation/désactivation des Label des stratégies
         - get_margins: retourne les valeurs des marges
-        - current_strategy(results=list):
+        - current_strategy(entries=list):
                 sélectionne les entités à présenter selon la stratégie
     """
 
@@ -50,13 +51,13 @@ class LoaderWindow(QDialog, Ui_ChargerFichier):
 
         """Adresses des fichiers"""
         self.classes_path = ''
-        self.results_path = ''
+        self.entries_path = ''
         self.orthoimage_path = ''
         self.footprint_path = ''
 
         """Connexions pour charger les fichiers"""
         self.chargerClasseButton.clicked.connect(self.select_classes)
-        self.chargerResultButton.clicked.connect(self.select_results)
+        self.chargerResultButton.clicked.connect(self.select_entries)
         self.chargerOrthoButton.clicked.connect(self.select_background)
         self.chargerEmpriseButton.clicked.connect(self.select_buildings_dir)
 
@@ -85,16 +86,16 @@ class LoaderWindow(QDialog, Ui_ChargerFichier):
         if self.classes_path:
             self.chemClasseLabel.setText(self.classes_path)
 
-    def select_results(self):
-        self.results_path, test = QFileDialog.getOpenFileName(
+    def select_entries(self):
+        self.entries_path, test = QFileDialog.getOpenFileName(
             self,
             "Sélection des résultats de la classification",
             "",
             "Fichiers CSV(*.csv)",
             options=QFileDialog.Options()
         )
-        if self.results_path:
-            self.cheminResultLabel.setText(self.results_path)
+        if self.entries_path:
+            self.cheminResultLabel.setText(self.entries_path)
 
     def select_background(self):
         self.orthoimage_path, test = QFileDialog.getOpenFileName(
@@ -125,16 +126,16 @@ class LoaderWindow(QDialog, Ui_ChargerFichier):
             )
         )
 
-    def current_strategy(self, results):
+    def current_strategy(self, entries):
         strat = [
             cle for cle in lib.strategy.STRATEGIES.keys()
             if cle == self.modeComboBox.currentText()
         ]
 
         if strat[0] == 'Naive':
-            return(lib.strategy.Naive().filter(results))
+            return(lib.strategy.Naive().filter(entries))
         if strat[0] == 'Random':
-            return(lib.strategy.Random(int(self.nbrEdit.text())).filter(results))
+            return(lib.strategy.Random(int(self.nbrEdit.text())).filter(entries))
 
 
 class CorrectionWindow(QDialog, Ui_ChoixClasse):
@@ -176,30 +177,17 @@ class CorrectionWindow(QDialog, Ui_ChoixClasse):
             return ''
 
 
-class MainWindow(QMainWindow, Ui_InterfacePrincipale):
+class MainWindow(QMainWindow, Ui_mainWindow):
     """
-    INTERFACE PRINCIPALE
+        Main Window
 
-    HERITAGE: Boite de dialogue "InterfacePrincipale"
-    ==========
+        Extends both `QMainWindow` and `Ui_mainWindow`.
 
-    ATTRIBUTS PRINCIPAUX :
-    ======================
-        - classes: dictionnaire des classes et de leur description.
-        - results: liste de 4-tuples avec les résultats de classification.
-        - output_buildings: liste d'objets Building après validation.
-        - new_label: classe sélectionnée par l'utilisateur.
-        - current: objet Building en cours de visualisation.
-
-    METHODES:
-    ==========
-        - show_correction_window: affiche la fenêtre de correction.
-        - show_loading_window: affiche la fenêtre de chargement et lis les données.
-        - show_building: affiche des emprises et de l'orthoimage dans l'espace graphique.
-        - validate: enregistre le Building validé par l'utilisateur.
-        - correct: enregistre le Building corrgié par l'utilisateur.
-        - save: sélectionne le chemin d'enregistrement et créer le fichier de sortie.
-        - next: parcourt les entités.
+        Attribute `classes` represents the classes that can have an instance.
+        Attribute `entries` is the list containing all instances.
+        Attribute `output_buildings` lists the program output.
+        Attribute `new_label` is the new label entered by the user.
+        Attribute `current` holds the currently processed building.
     """
 
     def __init__(self):
@@ -207,15 +195,16 @@ class MainWindow(QMainWindow, Ui_InterfacePrincipale):
         self.setupUi(self)
 
         self.classes = {}
-        self.results = []
-        self.output_buildings = []
+        self.entries = []
+        self.output_buildings = self.entries
 
         self.new_label = None
         self.current = None
 
-        self.chargerAction.triggered.connect(self.show_loading_window)
-        self.yesButton.clicked.connect(self.validate)
-        self.noButton.clicked.connect(self.correct)
+        self.loaderAction.triggered.connect(self.show_loading_window)
+        self.actionExit.triggered.connect(self.close)
+        self.validateButton.clicked.connect(self.validate)
+        self.correctButton.clicked.connect(self.correct)
 
     def show_correction_window(self):
         choix = CorrectionWindow()
@@ -228,7 +217,7 @@ class MainWindow(QMainWindow, Ui_InterfacePrincipale):
 
     def show_building(self):
         scene = QGraphicsScene(self)
-        self.entiteView.setScene(scene)
+        self.instanceView.setScene(scene)
 
         # Affichage de l'othoimage rognées
         item = scene.addPixmap(
@@ -252,11 +241,11 @@ class MainWindow(QMainWindow, Ui_InterfacePrincipale):
                 )
             scene.addPolygon(poly)
 
-        self.entiteView.fitInView(item, PyQt5.QtCore.Qt.KeepAspectRatio)
+        self.instanceView.fitInView(item, PyQt5.QtCore.Qt.KeepAspectRatio)
 
         # Affichage du texte
         self.idLabel.setText("Identitifiant: " + self.current.identity)
-        self.classeLabel.setText("Classe: " + self.current.classe)
+        self.classLabel.setText("Classe: " + self.current.classe)
         self.probaLabel.setText(
             "Probabilité: " + str(self.current.probability)
         )
@@ -266,10 +255,10 @@ class MainWindow(QMainWindow, Ui_InterfacePrincipale):
         bornes = self.background.get_crop_points(
             self.current.get_bounding_box()
         )
-        self.bornexValueLabel.setText(
+        self.boundxValueLabel.setText(
             '{:0.2f}, {:0.2f}'.format(bornes[0][0], bornes[0][1])
         )
-        self.borneyValueLabel.setText(
+        self.boundyValueLabel.setText(
             '{:0.2f}, {:0.2f}'.format(bornes[1][0], bornes[1][1])
         )
 
@@ -298,17 +287,17 @@ class MainWindow(QMainWindow, Ui_InterfacePrincipale):
     def save(self):
 
         # Sélection du chemin d'enregistrement
-        self.save_results_path, test = QFileDialog.getSaveFileName(
+        self.save_entries_path, test = QFileDialog.getSaveFileName(
             self,
             "Création du fichier de sauvegarde",
-            "output_results.csv",
+            "output_entries.csv",
             "Fichiers CSV(*.csv)",
             options=QFileDialog.Options()
         )
 
         # Lecture du fichier csv et remplissage avec output_buildings
-        if self.save_results_path:
-            with open(self.save_results_path, 'w', newline='') as save_file:
+        if self.save_entries_path:
+            with open(self.save_entries_path, 'w', newline='') as save_file:
                 output_writer = csv.writer(
                     save_file, delimiter=',', quoting=csv.QUOTE_MINIMAL
                 )
@@ -338,7 +327,7 @@ class MainWindow(QMainWindow, Ui_InterfacePrincipale):
             and
             loader.footprint_path != ''
             and
-            loader.results_path != ''
+            loader.entries_path != ''
         ):
             # Lecture du fichier csv des classes et remplissage du dictionnaire
             with open(loader.classes_path, newline='') as cls_file:
@@ -349,18 +338,18 @@ class MainWindow(QMainWindow, Ui_InterfacePrincipale):
                     self.classes[row['Nom']] = row['Description']
 
             # Lecture des résultats de la classification
-            with open(loader.results_path, newline='') as resuts_file:
+            with open(loader.entries_path, newline='') as resuts_file:
                 reader = csv.DictReader(
                     resuts_file,
                     fieldnames=['ID', 'Classe', 'Proba']
                 )
                 for row in reader:
-                    self.results.append(
+                    self.entries.append(
                         (row['ID'], row['Classe'], row['Proba'])
                     )
 
             # Selection des entités à présenter
-            self.selected_results = loader.current_strategy(self.results)
+            self.selected_entries = loader.current_strategy(self.entries)
 
             # Création d'une liste d'objets Building
             self.input_buildings = [
@@ -370,7 +359,7 @@ class MainWindow(QMainWindow, Ui_InterfacePrincipale):
                     classe,
                     prob
                 )
-                for building_id, classe, prob in self.selected_results
+                for building_id, classe, prob in self.selected_entries
             ]
             # Chargement de l'orthoimage
             self.background = lib.model.Background.from_geotiff(
