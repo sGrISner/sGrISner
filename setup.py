d91c8c7e127be78f45fd691b44205136dd8a871a
Ethiy <oussama.ennafii@ign.fr>
2018-02-16 14:35:40 +0100

    setup mini bug

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author_email='oussama.ennafii@ign.fr',
     license='GNU GPL',
     packages=find_packages(exclude=['tests']),
-    scripts=['sgrisner']
+    scripts=['sGrISner'],
     install_requires=[
             'PyQt5',
             'gdal',
